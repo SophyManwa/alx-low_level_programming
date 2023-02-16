8865a83b693a912387e78ff2f477bb1d5035c190
SophyManwa <sophykwamboka24@gmail>
2023-02-16 19:10:06 +0000

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-/*Puts function */
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
