a4fdb2a4a30a5897880dad90763847f702501960
SophyManwa <sophykwamboka24@gmail>
2023-02-17 19:34:01 +0000

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,17 +4,16 @@
 /**
  * main - entry point
  * Description: prints if the last digit of a string follows the set conditions.
- * Return: 0
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	int n;
+	int n, m;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	m = n % 10;
-
 	if(m > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, m);
