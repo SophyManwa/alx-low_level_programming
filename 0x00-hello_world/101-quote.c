7d4d9b3271d476e58eb243b24c9c58d0bf67389e
Sophy Manwa <48573222+SophyManwa@users.noreply.github.com>
2023-02-17 12:28:05 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - Entry point
  * Return: Always 1 (Success)
  */
+
 int main(void)
 {
-		fprintf(stderr, "and that piece of art is useful\"-");
-		fprintf(stderr, "Dora Korpar, 2015-10-19\n", 59);
+		fprintf(stderr, "and that piece of art is useful\" - ");
+		fprintf(stderr, "Dora Korpar, 2015-10-19\n");
+		
 		return (1);
 }
