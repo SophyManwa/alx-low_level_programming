dead4c96c7d6d9873cff8c87b4c2743fba824378
SophyManwa <sophykwamboka24@gmail>
2023-02-18 22:20:11 +0000

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,14 +8,14 @@ int main(void)
 {
 	int s, k;
 	/*for loop and if execution*/
-	for (s = 0; s < 10; s++)
+	for (s = 0; s < 9; s++)
 	{
 		for (k = s + 1; k < 10; k++)
 		{
 			putchar((s % 10) + '0');
 			putchar((k % 10) + '0');
 			/*if execution*/
-			if (s == 9 && k == 9)
+			if (s == 8 && k == 9)
 				continue;
 			putchar(',');
 			putchar(' ');
