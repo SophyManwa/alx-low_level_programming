eea33fb78e91e94ab548d495adeae0240ffa4457
SophyManwa <sophykwamboka24@gmail>
2023-02-21 18:02:59 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,7 @@
 #include "main.h"
-
 /**
  * main - Entry point
- * Description: prints 10x the alphabet in lowercase
+ * print_alphabet_10x - prints 10 times the alphabet in lowercase
  * Return: Always 0 (success)
  */
 void print_alphabet_x10(void)
