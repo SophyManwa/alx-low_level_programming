64631cd9a9e7da47a6fd1ed8a22717ae99ba52d7
SophyManwa <sophykwamboka24@gmail>
2023-02-21 19:21:18 +0000

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - function that prints a sign.
+ * @n: the int to check
+ *Return: 1 and prints + if n is greater than 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+	}
+	return (-1);
+}
