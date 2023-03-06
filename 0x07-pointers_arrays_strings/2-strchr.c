5f0e976cc9399dac8ea1b7896764cdaee851327c
SophyManwa <sophykwamboka24@gmail>
2023-03-06 17:37:33 +0000

    update

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,25 +3,18 @@
 /**
  * _strchr - locates a character in a string.
  * @s: the string to locate the character
- * @c: first occurrence
+ * @c: Character to be located
  *
  * Return: 0 if no character is found
  */
 char *_strchr(char *s, char c)
 {
 	int i = 0;
-	/* */
-	while (s != c)
+
+	for (; s[i] >= '\0'; i++)
 	{
-		++s;
-		++i;
+		if (s[i] == c)
+			return (s + i);
 	}
-	if (s == c)
-	{
-		return (s + i);
-	}
-	else
-	{
-		return (0);
-	}
+	return ('\0');
 }
