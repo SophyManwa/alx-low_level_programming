9bff3c7dbc3d21c1dd214f212187c0ade422faa0
SophyManwa <sophykwamboka24@gmail>
2023-03-06 19:47:04 +0000

    correction

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef main_h
 #define main_h
-
+int _putchar(char c);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
