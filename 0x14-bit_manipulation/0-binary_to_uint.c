96ba5efa9481e7ca9bd9348fbd376222d3eec1d2
SophyManwa <sophykwamboka24@gmail>
2023-04-11 17:22:57 +0000

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,16 +9,16 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
-	int l = 0;
+	int len = 0;
 
-	if (b[l] == '\0')
+	if (b[len] == '\0')
 		return (0);
 
-	while ((b[l] == '0') || (b[l] == '1'))
+	while ((b[len] == '0') || (b[len] == '1'))
 	{
 		num <<= 1;
-		num += b[l] - '0';
-		l++;
+		num += b[len] - '0';
+		len++;
 	}
 
 	return (num);
