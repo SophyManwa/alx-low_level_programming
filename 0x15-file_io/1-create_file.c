f0c14d9d5e9cf26a8986b32f8719b320ac4c64c9
SophyManwa <sophykwamboka24@gmail>
2023-04-11 17:07:44 +0000

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,14 +8,14 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, l = 0;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[l];)
+		for (len = 0; text_content[len];)
 			len++;
 	}
 
