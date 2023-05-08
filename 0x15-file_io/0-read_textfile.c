73436df79e0137d07c7f7beba412d2e5656afabf
SophyManwa <sophykwamboka24@gmail.com>
2023-05-08 19:05:23 +0000

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,40 +10,26 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *to_print;
-	ssize_t r, w;
-	int file;
+int fd, rd, wr;
+char *buf;
 
-	if (filename == NULL)
-		return (0);
-	to_print = malloc(sizeof(char) * letters);
-	if (to_print == NULL)
-		return (0);
-	file = open(filename, O_RDONLY);
-	if (file < 0)
-	{
-		free(to_print);
-		return (0);
-	}
-	r = read(file, to_print, letters);
-	if (r < 0)
-	{
-		free(to_print);
-		return (0);
-	}
-	if (r > 0)
-		w = write(STDOUT_FILENO, to_print, r);
-	if (w < r)
-	{
-		free(to_print);
-		return (0);
-	}
-	w = close(file);
-	if (w < 0)
-	{
-		free(to_print);
-		return (0);
-	}
-	free(to_print);
-	return (r);
+buf = malloc(sizeof(char) * letters);
+if (buf == NULL || filename == NULL)
+return (0);
+
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
+
+rd = read(fd, buf, letters);
+if (rd == -1)
+return (0);
+
+wr = write(STDOUT_FILENO, buf, rd);
+if (wr == -1)
+return (0);
+
+close(fd);
+free(buf);
+return (wr);
 }
